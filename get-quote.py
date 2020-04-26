0439c7763aa958fc2fb190ce6fff6604ae2b03cc
joonakor-uef <63606051+joonakor-uef@users.noreply.github.com>
2020-04-26 18:33:45 +0300

    Printed quote now selected at random.

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
-def primary():
-  print("Keep it logically awesome.")
+import random
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+def main():
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+
+  print(quotes[rnd])
 
 if __name__== "__main__":
-  primary()
+  main()
